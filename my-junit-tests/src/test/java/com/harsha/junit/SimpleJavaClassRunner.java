590b84eb5f411ded493696283f05f6e8a122d54b
Harsha Narayana <itsharshanarayana@gmail.com>
2018-05-05 15:14:08 -0400

    Added SimpleJavaClassRunner class.

---
+++ my-junit-tests/src/test/java/com/harsha/junit/SimpleJavaClassRunner.java
@@ -0,0 +1,19 @@
+package com.harsha.junit;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class SimpleJavaClassRunner {
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(SimpleJavaClassTest.class);
+		
+		for(Failure failure: result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+		
+		System.out.println(result.wasSuccessful());
+	}
+
+}
